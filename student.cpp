87fc40d3dc0263b2996d03ac2a0f7b62832c1387
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-17 03:43:02 +0530

    3

--- student.cpp
+++ student.cpp
@@ -47,3 +47,48 @@ string askLine(const string& prompt) {
     getline(cin, s);
     return s;
 }
+void Registration(Student& s) { 
+    cout << "\nRegistration Form \n";
+    if (s.name.empty())  s.name  = askLine("Enter student name: ");
+    if (s.program.empty()) s.program = askLine("Enter Department: ");
+
+    cout << "Complete Registration Process\n";
+    cout << "Form has sent  to corresponding departments\n";
+    cout << "Saved to Student Information DB \n";
+
+}
+
+void AdmissionOffice(Student& s) { 
+    cout << "\n Admission Office   Verification \n";
+    cout << "Receive form for " << s.name << " (" << s.program << ")\n";
+    do {
+        cout << "Checking details\n";
+        s.verified = askYesNo("Are details verified?");
+        if (!s.verified) {
+            cout << "Return form for correction. Please correct details\n";
+            // allow edits if user wants
+            if (askYesNo("Edit name?"))    s.name    = askLine("New name: ");
+            if (askYesNo("Edit program?")) s.program = askLine("New program: ");
+        }
+    } while (!s.verified);
+
+    cout << "Verified\n";
+}
+
+
+void Visa(Student& s) { 
+    cout << "\nVisa \n";
+    s.needsVisa = askYesNo("Need visa?");
+    if (s.needsVisa) {
+        cout << "Inform student for Visa Application.\n";
+        do {
+            s.visaApplied = askYesNo("Has the visa been applied?");
+            if (!s.visaApplied) cout << "Waiting until visa applied.\n";
+        } while (!s.visaApplied);
+        cout << "Visa application acknowledged.\n";
+    } else {
+        cout << "No visa required.\n";
+    }
+    
+}
+
